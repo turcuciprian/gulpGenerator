5a952bbb583ce8ba12cf38c83e004903be8aa85f
Ciprian Turcu <turcuciprian1@gmail.com>
2017-09-11 10:04:28 +0300

    added bootstrap

--- webpack.config.js
+++ webpack.config.js
@@ -3,58 +3,63 @@ const webpack = require('webpack');
 var BrowserSyncPlugin = require('browser-sync-webpack-plugin');
 
 module.exports = {
-  context: path.resolve(__dirname, 'src'),
-  entry: {
-    app: ['./app.js']
-  },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-  },
-  plugins: [
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'commons',
-      filename: 'commons.js',
-      minChunks: 2,
-    }),
-    new BrowserSyncPlugin({
-      // browse to http://localhost:3000/ during development,
-      // ./public directory is being served
-      host: 'localhost',
-      port: 3000,
-      server: {
-        baseDir: ['']
-      },
-    })
-  ],
-  devServer: {
-    contentBase: path.resolve(__dirname, './'),
-  },
-  module: {
-    rules: [{
-        test: /\.js$/,
-        exclude: [/node_modules/],
-        use: [{
-          loader: 'babel-loader',
-          options: {
-            presets: ['es2015']
-          },
-        }],
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(sass|scss)$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          'sass-loader',
-        ]
-      }
-
-      // Loaders for other file types can go here
+    context: path.resolve(__dirname, 'src'),
+    entry: {
+        app: ['./app.js']
+    },
+    watch: true,
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+    plugins: [
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'commons',
+            filename: 'commons.js',
+            minChunks: 2,
+        }),
+        new BrowserSyncPlugin({
+            // browse to http://localhost:3000/ during development,
+            // ./public directory is being served
+            host: 'localhost',
+            port: 3000,
+            server: {
+                baseDir: ['']
+            },
+        })
     ],
-  },
+    devServer: {
+        contentBase: path.resolve(__dirname, './'),
+    },
+    module: {
+        rules: [{
+                test: /\.js$/,
+                exclude: [/node_modules/],
+                use: [{
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['es2015']
+                    },
+                }],
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(sass|scss)$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader',
+                ]
+            },
+            {
+                test: /\.(png|woff|woff2|eot|ttf|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                loader: 'url-loader'
+            }
+
+            // Loaders for other file types can go here
+        ],
+    },
 };
