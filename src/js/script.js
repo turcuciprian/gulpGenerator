fb2b0f994d4873cd12f43f93b8d6f1c4ba0f3aed
iaonachi <ioana_chichi@outlook.com>
2017-09-22 00:50:05 +0300

    stuff

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,12 @@
 import Vue from 'vue';
 import * as uiv from 'uiv';
+Vue.component('vueTypeahead', require('vuejs-autocomplete'));
+
 Vue.use(uiv)
 
 new Vue({
-  el:'#app',
-  data:{
+  el: '#app',
+  data: {
     form: {
       name: '',
       version: '',
@@ -15,9 +17,39 @@ new Vue({
       repositoryURL: '',
       bugsURL: '',
       homepage: '',
-    }
+      devDependencies: '',
+      dependencies: '',
+    },
+    function() {
+      return {
+        label: '',
+        value: '',
+        value1: '',
+        myTemplate: '<div><h3>{{team}}</h3><h4>Custom Template</h4></div>',
+        localValues: ['Dhaka', 'Rangpur', 'Rajshahi', 'Sylhet', 'Khulna']
+      }
+    },
   },
   components: {
     uiv
+  },
+  methods: {
+    done: function(data) {
+      console.log(data);
+    }
   }
+});
+
+const app = new Vue({
+  el: '#app',
+  data: function() {
+    return {
+      label: '',
+      value: '',
+      value1: '',
+      myTemplate: '<div><h3>{{team}}</h3><h4>Custom Template</h4></div>',
+      localValues: ['Dhaka', 'Rangpur', 'Rajshahi', 'Sylhet', 'Khulna']
+    }
+  },
+
 });
